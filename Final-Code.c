9a5520c20b144d8c07088ac1ed50f04d8a009079
pranav245 <48914365+pranav245@users.noreply.github.com>
2019-04-10 17:25:22 +0530

    Update and rename p1.c to Final-Code.c

--- Final-Code.c
+++ Final-Code.c
@@ -2,15 +2,20 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<pthread.h>
+#include<time.h>
 
-pthread_mutex_t read1,write1,flag1;
+time_t t;// declaration to catch the current time
+ 
+pthread_mutex_t read1,write1,flag1; // decalration of mutex variable
 
-int read_count=0;
-int glob=1,flag=0;
-int rcg=0,wcg=0;
+int read_count=0; // this is used to count number of readers
+int glob=1;   // shared data
 
-    pthread_t readid[50], writeid[50];
+int flag=0,rcg=0,wcg=0;  // helping variables
 
+int a1[25],a2[25],a3[25],a4[25],a5[25];
+
+//Reader function    
 void *reader(void *arg){
 	
             int i=(*(int*)arg);                      
@@ -29,7 +34,10 @@ void *reader(void *arg){
                 pthread_mutex_lock(&flag1);
                 printf("\n\n\n\n\t# READER_%d \n",rcg);
                 printf("\tNumber of reader at this time is %d\n",read_count);
-                printf("\tData read by the reader_%d is %d\n",rcg++,glob);    
+                a4[rcg]=glob;                
+                printf("\tData read by the reader_%d is %d\n",rcg++,glob);
+                time(&t);
+                printf("\tCurrent time is : %s",ctime(&t));    
                 printf("\tThread will terminate after %d seconds\n",i);             
                 pthread_mutex_unlock(&flag1);    
                 
@@ -46,6 +54,7 @@ void *reader(void *arg){
        
 }
 
+//Writer function
 void *writer(void *arg){
         int i=(*(int*)arg);
     
@@ -58,9 +67,13 @@ void *writer(void *arg){
                  
             printf("\n\n\n\n\t# WRITER_%d \n",wcg);
             printf("\tThe number of readers at this time is %d\n",read_count);
+            
             printf("\tData read by the writer_%d is %d\n",wcg++,glob);
             glob=(rand())%100;
+            a5[wcg-1]=glob;            
             printf("\tNow the Writer has updated the shared data\n\tNow Data = %d\n",glob);
+            time(&t);
+            printf("\tCurrent time is : %s",ctime(&t));                    
             printf("\tThread will terminate after %d seconds\n",i);
             pthread_mutex_unlock(&flag1);    
             sleep(i);
@@ -83,7 +96,59 @@ void mainmenu(int num)
     printf("\t ------------------------------------- \n\n\t");   
     
    
+ }
+
+void table1(int num,int opt, int t)
+{   
+    
+        a1[num]=num;
+        a2[num]=t;
+        a3[num]=opt;
+    
+    
+        
+    printf("\n\t\t-------------------------------------\n");
+    printf("\t\t| Thread Number |    Type   |  Time   |\n");
+    printf("\t\t---------------------------------------\n");
+        
+    for(int i=0;i<num+1;i++)
+    {
+    if(a3[i]==1)
+    {
+    printf("\t\t|   Thread %d   |  Writing  |  %d sec |\n",a1[i],a2[i]);
+    printf("\t\t---------------------------------------\n");    
     }
+    else if(a3[i]==2)
+    {
+    printf("\t\t|   Thread %d   |  Reading  |  %d sec |\n",a1[i],a2[i]);
+    printf("\t\t---------------------------------------\n");    
+    }
+    }
+
+}
+void table2(int num)
+{   
+
+    printf("\n\t\t------------------------------------------------------\n");
+    printf("\t\t| Thread Number |    Type   |  Time   |   Operation   | \n");
+    printf("\t\t------------------------------------------------------\n");
+        
+    for(int i=0,j=0,k=0;i<num;i++)
+    {
+    if(a3[i]==1)
+    {
+    printf("\t\t|   Thread %d    |  Writing  |  %d sec | Data write:%d |\n",a1[i],a2[i],a5[j++]);
+    printf("\t\t------------------------------------------------------\n");    
+    }
+    else if(a3[i]==2)
+    {
+    printf("\t\t|   Thread %d    |  Reading  |  %d sec | Data read:%d  |\n",a1[i],a2[i],a4[k++]);
+    printf("\t\t------------------------------------------------------\n");    
+    }
+    }
+    printf("\n\n");
+
+}
 
 
 
@@ -92,35 +157,44 @@ int main()
     pthread_mutex_init(&read1,NULL);
     pthread_mutex_init(&write1,NULL);
     pthread_mutex_init(&flag1,NULL);
+    pthread_t readid[50], writeid[50];
+
     int array1[50];
     int array2[50];
     int array3[100];
     int thread_count=0;
+
+    
+    int rc=0,wc=0,tc=0,sum=0,z=0,time1,option=0,sl=0,ic=0,jc=0;
+    char c;
+    system("clear");
     
     printf("\n\tThe Shared data between reader and write is initially %d\n\n\n",glob);
     
     printf("\t#Note\n\tChoose the task you want to perform with the respective thread.\n");
     printf("\tOnce you done with input press 3 to start all the threads to do their \n\t -respective tasks\n");
+    
+    printf("\n\tNumber of threads shouls not be more than 50");
 
     printf("\n\tPress Enter to countinue");
-    int c;    
+    
     scanf("%c",&c);
         
     system("clear");    
-    int rc=0,wc=0,tc=0,sum=0,z=0,time;
+       
     while(1>0)
     {   
         mainmenu(thread_count);
-        int option;
+        
         scanf("%d",&option);
         
         if(option==2)
         {   z=2;
             array3[tc++]=2;
             printf("\n\tEnter running time for this thread: ");
-            scanf("%d",&time);            
-            array2[rc++]=time;
-          //  sum+=time;
+            scanf("%d",&time1);            
+            array2[rc++]=time1;
+          //  sum+=time1;
             printf("\n");                   
             thread_count++;        
         }
@@ -129,9 +203,11 @@ int main()
         {   z=1;
             array3[tc++]=1;
             printf("\n\tEnter running time for this thread:");
-            scanf("%d",&time);
-            array1[wc++]=time;
-            sum+=time;
+            scanf("%d",&time1);
+            if(wc==0)
+                sl=time1;
+            array1[wc++]=time1;
+            sum+=time1;
             printf("\n");            
             
             thread_count++;      
@@ -148,16 +224,14 @@ int main()
         else
         {
             printf("\n\n\n\tOOPSS!!!!!!!\nEnter appropriate choice \n\n");
-            sleep(2);        
+            sleep(1);        
         }
     sleep(0.5);
     system("clear");
-       
+    table1((thread_count-1),z,time1);  
     }
     
-            
-        
-            int ic=0,jc=0;
+               
 
         if(array3[0]==1)
             pthread_create(&writeid[0],NULL,writer,(void*)&array1[ic++]);
@@ -168,7 +242,7 @@ int main()
             
        
 
-         for(int b=jc;b<rc;b++)
+        for(int b=jc;b<rc;b++)
                 pthread_create(&readid[b],NULL,reader,(void*)&array2[jc++]);                                                    
 
         for(int b=ic;b<wc;b++)
@@ -177,12 +251,13 @@ int main()
         for(int b=0;b<rc;b++)
                 pthread_join(readid[b],NULL);
         
-        for(int b=0;b<rc;b++)
+        for(int b=0;b<wc;b++)
                 pthread_join(writeid[b],NULL);
 
- 
-     // sleep(sum);
         
-        //printf("\n\tPress Enter to exit\n");
-        scanf("\n\n%c",&c);
-}
+        sleep(sum);
+        
+        
+        table2(thread_count);
+        
+ }
